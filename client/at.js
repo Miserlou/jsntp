e433521d4421b6c06761a1a3f9cd5e0d142059aa
Thomas Levine <thomas.levine@gmail.com>
2011-09-06 18:13:50 -0400

    package.json

--- client/at.js
+++ client/at.js
@@ -2,7 +2,7 @@
    Schedule things to happen at certain times
 
    By Thomas Levine, built on the substantial shoulders
-   of his team at the HTML5 Hackathon at Google Kirkland
+   of his group at the HTML5 Hackathon at Google Kirkland
 */
 
 at = {
--- client/ntp.js
+++ client/ntp.js
@@ -1,3 +1,10 @@
+/*
+   Schedule things to happen at certain times
+
+   By Thomas Levine, built on the substantial shoulders
+   of his group at the HTML5 Hackathon at Google Kirkland
+*/
+
 ntp={
   'offsets':new Array()
 , 'math':{
