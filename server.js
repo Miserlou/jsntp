f508376c8c2ee0ac9f35ea892a529126d2694ddd
Thomas Levine <thomas.levine@gmail.com>
2011-09-06 14:18:34 -0400

    make pages load

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 ntp={
-  'WEBDIR':'/ntp/'
-, 'SRVDIR':'client/'
+  'WEBDIR':'ntp'
+, 'SRVDIR':'client'
 , 'CLIENT':'ntp.js'
 
 , 'listen':function (app) {
@@ -17,17 +17,17 @@ ntp={
 
 , 'client':function (request,response) {
       //If the ntp directory is requested, do something
-      if (request.url.split('/')[1]===this.DIR){
-        if (request.url===this.WEBDIR+this.CLIENT) {
-          var filePath = this.SRVDIR+this.CLIENT;
-          fs.readFile(filePath, function(error, content) {
-    	if (error) {
-    	  response.writeHead(500);
-    	  response.end();
-    	} else {
-    	  response.writeHead(200, {'Content-Type':'text/javascript'});
-    	  response.end(content, 'utf-8');
-    	}
+      if (request.url.split('/')[1]===this.WEBDIR){
+        if (request.url==='/'+this.WEBDIR+'/'+this.CLIENT) {
+          var filePath = this.SRVDIR+'/'+this.CLIENT;
+          this.fs.readFile(filePath, function(error, content) {
+            if (error) {
+              response.writeHead(500);
+              response.end('Oops. The ntp.js installation is apparently messed up.');
+            } else {
+              response.writeHead(200, {'Content-Type':'text/javascript'});
+              response.end(content, 'utf-8');
+            }
           });
         } else {
           response.writeHead(500);
@@ -39,14 +39,16 @@ ntp={
 }
 
 //Calling this library
+
 function handler (request,response) {
   ntp.client(request,response)
   /* Your code here
      ...
   */
 }
-var app = require('http').createServer(handler)
 
-var app = require('http').createServer(ntp.client) //If you're only synchronizing
+//var app = require('http').createServer(ntp.client) //If you're only synchronizing
+var app = require('http').createServer(handler) 
+
 app.listen(8000);
 ntp.listen(app);
