651785f6cf2bd4a30ea18f7c6a5dba54e2200e12
Thomas Levine <thomas.levine@gmail.com>
2011-09-06 13:38:30 -0400

    rearraneg server

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-  , path = require('path')
+function ntplisten(app) {
+  var io = require('socket.io').listen(app)
+    , fs = require('fs')
+    , path = require('path')
 
-var WEBDIR='/ntp/'
-  , SRVDIR='client/'
-  , CLIENT='ntp.js'
+  var WEBDIR='/ntp/'
+    , SRVDIR='client/'
+    , CLIENT='ntp.js'
 
-var ERROR='Welcome to NTP.js'
+  var ERROR='Welcome to NTP.js'
 
-app.listen(8000);
-
-function handler (request,response) {
-  //If the ntp directory is requested, do something
-  if (request.url.split('/')[1]===DIR){
-    if (request.url===DIR+CLIENT) {
-      var filePath = SRVDIR+CLIENT;
-      fs.readFile(filePath, function(error, content) {
-        if (error) {
-          response.writeHead(500);
-          response.end();
-        } else {
-          response.writeHead(200, {'Content-Type':'text/javascript'});
-          response.end(content, 'utf-8');
-        }
-      });
-    } else {
-      response.writeHead(500);
-      response.end(ERROR);
+  function handler (request,response) {
+    //If the ntp directory is requested, do something
+    if (request.url.split('/')[1]===DIR){
+      if (request.url===DIR+CLIENT) {
+        var filePath = SRVDIR+CLIENT;
+        fs.readFile(filePath, function(error, content) {
+  	if (error) {
+  	  response.writeHead(500);
+  	  response.end();
+  	} else {
+  	  response.writeHead(200, {'Content-Type':'text/javascript'});
+  	  response.end(content, 'utf-8');
+  	}
+        });
+      } else {
+        response.writeHead(500);
+        response.end(ERROR);
+      }
     }
   }
+
+  io.sockets.on('connection', function (socket) {
+    socket.on('message', function (clientTime) {
+      socket.send(new Date().getTime()+':'+clientTime);
+    });
+  });
 }
 
-io.sockets.on('connection', function (socket) {
-  socket.on('message', function (clientTime) {
-    socket.send(new Date().getTime()+':'+clientTime);
-  });
-});
+//Calling this library
+var app = require('http').createServer(handler)
+app.listen(8000);
+ntplisten(app)
